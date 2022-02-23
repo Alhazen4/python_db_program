78f9a558ed796c052dfca4a11543ebb97d34f71f
Alhazen4 <62123569+Cybernonk404@users.noreply.github.com>
2022-02-23 20:45:29 +0700

    Update create table and show table data

--- dbprog.py
+++ dbprog.py
@@ -14,13 +14,28 @@ choice = input("Your choice: ")
 
 def tryConnDB():
     dbfunc._init_(dbfunc, 'host', '3306', 'root', config('PASSDB'))
-    dbfunc.check_conn(dbfunc)
     dbfunc.connect_db(dbfunc)
 
 if choice == "1":
-    tryConnDB();
+    tryConnDB()
 
 elif choice == "2":
-    tryConnDB();
+    tryConnDB()
     dbname = input("Input your new database name: ")
     dbfunc.create_db(dbfunc, dbname)
+
+elif choice == "3":
+    tryConnDB()
+    dbname = input("Input the name of database: ")
+    tbname = input("Input the name of table: ")
+    dbfunc.create_table(dbfunc, dbname, tbname)
+
+elif choice == "4":
+    tryConnDB()
+    dbname = input("Input the name of database: ")
+    tbname = input("Input the name of table: ")
+    dbfunc.load_data(dbfunc, dbname, tbname)
+
+elif choice == "5":
+    tryConnDB()
+    
